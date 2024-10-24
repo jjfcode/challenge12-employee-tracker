8c20fca5e2a84b7699a920f1fccf4a1e1af5c981
John Ferlito <johnferlito73@gmail.com>
2024-10-24 18:38:06 -0400

    complete schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,16 @@ CREATE TABLE department (
   name VARCHAR(30) UNIQUE NOT NULL
 );
 
+CREATE TABLE role (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER NOT NULL
+);
+
+CREATE TABLE employee (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  role_id INTEGER NOT NULL,
+  manager_id INTEGER
+);
