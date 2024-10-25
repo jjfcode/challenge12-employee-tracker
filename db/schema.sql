8a72e96681cf220771f18059d6c4f740b5ab23e5
John Ferlito <johnferlito73@gmail.com>
2024-10-24 21:56:05 -0400

    schema finish

--- db/schema.sql
+++ db/schema.sql
@@ -11,13 +11,25 @@ CREATE TABLE department (
 CREATE TABLE role (
   id SERIAL PRIMARY KEY,
   title VARCHAR(30) UNIQUE NOT NULL,
-  salary DECIMAL NOT NULL,
-  department_id INTEGER NOT NULL
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INTEGER NOT NULL,
+  FOREIGN KEY(department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
   id SERIAL PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
   role_id INTEGER NOT NULL,
-  manager_id INTEGER
+  manager_id INTEGER,
+
+  FOREIGN KEY(role_id)
+  REFERENCES role(id)
+  ON DELETE SET NULL,
+
+  FOREIGN KEY(manager_id)
+  REFERENCES employee(id)
+  ON DELETE SET NULL
 );
