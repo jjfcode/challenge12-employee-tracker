d8ae3f3cba3c259368f8c01fff47c6902408219e
John Ferlito <johnferlito73@gmail.com>
2024-10-29 21:37:20 -0400

    fixing a bug

--- index.js
+++ index.js
@@ -33,30 +33,78 @@ function mainMenu(){
         else if (response.menu === 'view all employees'){
             viewEmployees()
         }
-        else if (response.menu === 'add an employee')
+        else if (response.menu === 'add an employee') {
             addEmployee()
+        }
+        else if (response.menu === 'update an employee role') {
+            updateEmployeeRole()
+        }
     })
 }
 
-function addEmployee(){
-    pool.query('SELECT title as name, id as value')
-    inquirer.prompt([
-        {
-            type:'input',
-            message:"What is the employee's first name?",
-            name:'first_name'
-        },
-        {
-            type:'input',
-            message:"What is the employee's last name?",
-            name:'last_name'
-        },
-        {
-            type:'list',
-            message:"What is the employee's role?",
-            name:'role'
-        }
-    ])
+function updateEmployeeRole(){
+    pool.query("SELECT CONCAT(first_name,' ',last_name) as name, id as value from employee",(err,{ rows }) => {
+        pool.query("SELECT title as name, id as value from role", (err, { rows:roleRows }) => {
+            inquirer.prompt([
+                {
+                    type:'list',
+                    message:"Which employee's do you want to update the role?",
+                    name:'employee',
+                    choices:rows
+                },
+                {
+                    type:'list',
+                    message:"Which role do you want to assign to the selected employee?",
+                    name:'role',
+                    choices:roleRows
+                }
+            ])
+            .then(res =>{
+                pool.query(`update omployee set role_id = ${res.role} where id=${res.employee}`,(err)=>{
+                    console.log("Employee's role has been updated!")
+                    viewEmployees()
+                })
+            })
+        })
+    })
+}
+
+function addEmployee() {
+    pool.query('SELECT title as name, id as value',(err,{ rows }) => {
+        pool.query("SELECT CONCAT(first_name,' ',last_name) as name, id as value from employee", (err, { rows:managerRows }) => {
+            inquirer.prompt([
+                {
+                    type:'input',
+                    message:"What is the employee's first name?",
+                    name:'first_name'
+                },
+                {
+                    type:'input',
+                    message:"What is the employee's last name?",
+                    name:'last_name'
+                },
+                {
+                    type:'list',
+                    message:"What is the employee's role?",
+                    name:'role',
+                    choices:rows
+                },
+                {
+                    type:'list',
+                    message:"What is the employee's manager?",
+                    name:'manager',
+                    choices:managerRows
+                }
+            ])
+            .then(res=>{
+                pool.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${res.first_name}', '${res.last_name}', ${res.role}, ${res.manager})`,
+                    (err) => {
+                    console.log("New employee has been added into system!");
+                    viewEmployees();
+                });
+            });
+        });
+    });
 }
 
 function viewEmployees(){
@@ -68,7 +116,7 @@ ON role.id = employee.role_id
 LEFT JOIN department 
 ON department.id = role.department_id
 LEFT JOIN employee as employee_manager 
-ON employee.manager_id = employee_manager.id ORDER BY employee.id;`,(err,{rows})=>{
+ON employee.manager_id = employee_manager.id ORDER BY employee.id;`,(err,{ rows })=>{
     printTable(rows)
     mainMenu()
     })
